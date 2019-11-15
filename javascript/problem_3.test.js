945c8d0742fccf45d5409853e23d5219a98f7eb5
alimnastaev <a.nastaev@gmail.com>
2019-11-15 09:31:02 -0500

    added link to KhanAcademy where explains PF

--- javascript/problem_3.test.js
+++ javascript/problem_3.test.js
@@ -1,5 +1,6 @@
 //  The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
+// https://www.khanacademy.org/math/pre-algebra/pre-algebra-factors-multiples/pre-algebra-prime-factorization-prealg/v/prime-factorization
 // https://projecteuler.net/problem=3
 
 function largest_prime_factor(n) {
