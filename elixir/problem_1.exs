2b4f0a4b9ab06e44eb7684759d831129bdca6537
alimnastaev <a.nastaev@gmail.com>
2019-11-19 15:55:09 -0500

    [problem_1] renamed modules

--- elixir/problem_1.exs
+++ elixir/problem_1.exs
@@ -1,4 +1,4 @@
-defmodule ProblemOne do
+defmodule Problem1 do
   @doc """
   If we list all the natural numbers below 10 that are multiples of 3 or 5,
   we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -8,7 +8,7 @@ defmodule ProblemOne do
 
   3 solutions for this problem
   """
-
+  #####################
   def fizz_buzz_multiples(r) do
     Enum.reduce(1..r, 0, fn x, acc ->
       if rem(x, 3) == 0 or rem(x, 5) == 0 do
@@ -19,17 +19,28 @@ defmodule ProblemOne do
     end)
   end
 
+  # :timer.tc(&ProblemOne.fizz_buzz_multiples/1, [999])
+  # {39, 233168}
+
+  #####################
   # with Enum.sum
   # def fizz_buzz_multiples(r) do
   #   Enum.filter(1..r, fn n -> rem(n, 3) == 0 or rem(n, 5) == 0 end)
   #   |> Enum.sum()
   # end
 
+  # :timer.tc(&ProblemOne.fizz_buzz_multiples/1, [999])
+  # {72, 233168}
+
+  #####################
   # with Enum.reduce
-  #   def fizz_buzz_multiples(r) do
+  # def fizz_buzz_multiples(r) do
   #   Enum.filter(1..r, fn n -> rem(n, 3) == 0 or rem(n, 5) == 0 end)
   #   |> Enum.reduce(0, fn x, acc -> acc + x end)
   # end
+
+  # :timer.tc(&ProblemOne.fizz_buzz_multiples/1, [999])
+  # {72, 233168}
 end
 
 # IO.puts(ProblemOne.fizz_buzz_multiples(9))
--- elixir/test/problem_1_test.exs
+++ elixir/test/problem_1_test.exs
@@ -1,11 +1,11 @@
 ExUnit.start()
 
-defmodule ProblemOneTest do
+defmodule Problem1Test do
   use ExUnit.Case
 
   test "multiples 3 and 5" do
-    assert ProblemOne.fizz_buzz_multiples(9) == 23
-    assert ProblemOne.fizz_buzz_multiples(999) == 233168
-    refute ProblemOne.fizz_buzz_multiples(-5) == 23
+    assert Problem1.fizz_buzz_multiples(9) == 23
+    assert Problem1.fizz_buzz_multiples(999) == 233168
+    refute Problem1.fizz_buzz_multiples(-5) == 23
   end
 end
