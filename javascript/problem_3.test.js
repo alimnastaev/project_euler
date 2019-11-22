9f20ab45fb51a738dc7753ca557ea25f840b7b37
alimnastaev <a.nastaev@gmail.com>
2019-11-21 20:59:58 -0500

    [problem_3 JS] 2 solutions with tests

--- javascript/problem_3.test.js
+++ javascript/problem_3.test.js
@@ -2,8 +2,7 @@
 // What is the largest prime factor of the number 600851475143 ?
 // https://www.khanacademy.org/math/pre-algebra/pre-algebra-factors-multiples/pre-algebra-prime-factorization-prealg/v/prime-factorization
 // https://projecteuler.net/problem=3
-
-function largest_prime_factor(n) {
+function largest_prime_factor_1(n) {
   let array_of_numbers = []
   for (let f = 2; f < n * n; f++)
     if (Number.isInteger(n / f) || (n / f == 1)) {
@@ -14,26 +13,68 @@ function largest_prime_factor(n) {
   return Math.max(...array_of_numbers)
   // return array_of_numbers # if you want to see complete prime factors pattern
 }
+console.time('1st solution');
+largest_prime_factor_1(600851475143);
+console.timeEnd('1st solution');
+
+function largest_prime_factor_2(n) {
+  let f = 2
+  while (f * f <= n) {
+    if (n % f == 0) {
+      n /= f
+    } else {
+      f += 1
+    }
+  }
+  return n
+}
+
+console.time('2nd solution');
+largest_prime_factor_2(600851475143);
+console.timeEnd('2nd solution');
 
 
-describe('prime factor testing', () => {
+
+describe('1st function', () => {
   test('success case to prime factor for 13195', () => {
-    expect(largest_prime_factor(13195)).toEqual(29)
+    expect(largest_prime_factor_1(13195)).toEqual(29)
   })
 
   test('success case to prime factor 600851475143', () => {
-    expect(largest_prime_factor(600851475143)).toEqual(6857)
+    expect(largest_prime_factor_1(600851475143)).toEqual(6857)
   })
 
   test('success case to prime factor 1000', () => {
-    expect(largest_prime_factor(1000)).toEqual(5)
+    expect(largest_prime_factor_1(1000)).toEqual(5)
   })
 
   test('fail case if no argument provided', () => {
-    expect(largest_prime_factor()).not.toEqual(6857)
+    expect(largest_prime_factor_1()).not.toEqual(6857)
   })
 
   test('fail case if an argument is zero', () => {
-    expect(largest_prime_factor(0)).toEqual(-Infinity)
+    expect(largest_prime_factor_1(0)).toEqual(-Infinity)
+  })
+})
+
+describe('2nd function', () => {
+  test('success case to prime factor for 13195', () => {
+    expect(largest_prime_factor_2(13195)).toEqual(29)
+  })
+
+  test('success case to prime factor 600851475143', () => {
+    expect(largest_prime_factor_2(600851475143)).toEqual(6857)
+  })
+
+  test('success case to prime factor 1000', () => {
+    expect(largest_prime_factor_2(1000)).toEqual(5)
+  })
+
+  test('fail case if no argument provided', () => {
+    expect(largest_prime_factor_2()).not.toEqual(6857)
+  })
+
+  test('fail case if an argument is zero', () => {
+    expect(largest_prime_factor_2(0)).toEqual(0)
   })
 })
